5129130cf95133cb1d259cbb3250a4f7a4c6204a
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-10-04 01:06:46 +0200

    passaggio dati singolo store

--- src/components/StoreCard.vue
+++ src/components/StoreCard.vue
@@ -35,26 +35,24 @@ export default {
 </script>
 
 <template>
-    <div class="storeshow-visibility-container">
-        <div class="storeshow-info-container">
-            <div class="img-container" @click="redirectToStoreInfo">
-                <img :src="store.imageURL + this.singleStore.logo" @error="handleImageError" :alt="this.singleStore.logo">
-                <div class="store-card-overlay">
-                    <span>{{ this.singleStore.store_name }}</span>
-                </div>
+    <div class="storeshow-info-container">
+        <div class="img-container" @click="redirectToStoreInfo">
+            <img :src="store.imageURL + this.singleStore.logo" @error="handleImageError" :alt="this.singleStore.logo">
+            <div class="store-card-overlay">
+                <span>{{ this.singleStore.store_name }}</span>
             </div>
-            <div class="info-container">
-                <span class="affiliation-code">{{ this.singleStore.affiliation_code }}</span>
-                <span class="discount">{{ this.singleStore.discount }} %</span>
-            </div>
-            <h3>{{ this.singleStore.store_name }}</h3>
-            <span class="end-offer">Fine Offerta: <b>10 novembre 2023</b></span>
-            <hr>
         </div>
-        <a class="storeshow-button" :href="this.singleStore.link">
-            <span>Vai al sito</span>
-        </a>
+        <div class="info-container">
+            <span class="affiliation-code">{{ this.singleStore.affiliation_code }}</span>
+            <span class="discount">{{ this.singleStore.discount }} %</span>
+        </div>
+        <h3>{{ this.singleStore.store_name }}</h3>
+        <span class="end-offer">Fine Offerta: <b>10 novembre 2023</b></span>
+        <hr>
     </div>
+    <a class="storeshow-button" :href="this.singleStore.link">
+        <span>Vai al sito</span>
+    </a>
 </template>
 
 <style lang="scss" scoped></style>
--- src/router-view/StoreInfo.vue
+++ src/router-view/StoreInfo.vue
@@ -1,19 +1,49 @@
 <script>
+import axios from 'axios';
 import { store } from '../store';
+import AppHeader from '../components/AppHeader.vue';
+import AppFooter from '../components/AppFooter.vue';
 
 export default {
     name: 'StoreInfo',
+    components: {
+        AppHeader,
+        AppFooter
+    },
+
     data() {
         return {
-            store,
+            storeData: null,
         }
     },
-    methods: {
+
+    mounted() {
+        const storeId = this.$route.params.id;
+
+        axios.get(store.apiURL + '/store/' + storeId)
+            .then(response => {
+
+                this.storeData = response.data.StoreById;
+
+                console.log('Dati dello store:', this.storeData);
+            })
+            .catch(error => {
+
+                console.log('Errore nella richiesta API:', error);
+            });
+
 
     }
 }
 </script>
 
 <template>
-    <h1>hello world</h1>
+    <AppHeader />
+
+    <div class="store-info-container">
+        <h2>{{ this.storeData ? this.storeData.store_name : 'Caricamento in corso...' }}</h2>
+        <!-- Mostra il nome del negozio se i dati sono stati caricati, altrimenti mostra un messaggio di caricamento -->
+    </div>
+
+    <AppFooter />
 </template>
